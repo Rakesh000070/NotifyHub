d6be59f0c2102d3f65e3b9d9acc114025e7e933f
Rakesh Nayak <rakeshnayak2332341k@gmail.com>
2026-07-24 22:38:20 +0530

    day-4 complete

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,6 +1,8 @@
 // Import the User model.
 // We use this model to interact with the "users" collection in MongoDB.
 const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+
 
 // Controller function to handle user registration.
 // This function runs when a client sends a registration request.
@@ -14,22 +16,31 @@ const registerUser = async (req, res) => {
     //   "password": "123456"
     // }
     const { name, email, password } = req.body;
+    const existingUser = await User.findOne({ email });
 
+if (existingUser) {
+    return res.status(400).json({
+        success: false,
+        message: "User already exists try to login or use another email to successfully register",
+    });
+}
+    const hashedPassword = await bcrypt.hash(password, 10);
     // Create a new user document in the MongoDB database.
     // User.create() saves the data and returns the newly created document.
     const newUser = await User.create({
       name,
       email,
-      password,
+      password: hashedPassword,// Store the hashed password instead of the plain text password.
     });
+
+    newUser.password = undefined;// Hide the password field before sending the response.
 
     // If user creation is successful, send a success response.
     // Status Code: 201 = Resource Created Successfully.
     res.status(201).json({
       success: true,
       message: "User Registered Successfully",
-
-      // Return the created user data to the client.
+     // Return the created user data to the client.
       user: newUser,
     });
   } catch (error) {
